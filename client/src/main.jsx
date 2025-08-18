282d0aa8e83179dc215353343d2baed46e4c0eed
pwwongaa <ddwongdd111@gmail.com>
2025-08-18 20:03:12 +0100

    Fix FE

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { ErrorBoundary } from 'react-error-boundary'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
